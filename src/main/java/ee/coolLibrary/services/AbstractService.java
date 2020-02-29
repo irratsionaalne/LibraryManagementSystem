02be8988ea8406608e697a49bd734491034f4560
dmitrii <dmitrii.golikov91@gmail.com>
2020-02-29 14:45:44 +0200

    services author start

--- src/main/java/ee/coolLibrary/services/AbstractService.java
+++ src/main/java/ee/coolLibrary/services/AbstractService.java
@@ -3,7 +3,9 @@ package ee.coolLibrary.services;
 import ee.coolLibrary.entities.SimpleEntity;
 import ee.coolLibrary.repositories.SimpleRepository;
 
-public class AbstractService <REPO extends SimpleRepository<ENT, ID>, ENT extends SimpleEntity<ID>, ID> implements SimpleService <ENT, ID> {
+import java.util.Objects;
+
+public abstract class AbstractService <REPO extends SimpleRepository<ENT, ID>, ENT extends SimpleEntity<ID>, ID> implements SimpleService <ENT, ID> {
 
     REPO repository;
 
@@ -11,28 +13,5 @@ public class AbstractService <REPO extends SimpleRepository<ENT, ID>, ENT extend
         this.repository = repository;
     }
 
-    @Override
-    public ENT save(ENT entity) {
-        return repository.save(entity);
-    }
 
-    @Override
-    public ENT findById(ID id) {
-        return repository.findById(ENT, id);
-    }
-
-    @Override
-    public ENT delete(ENT entity) {
-        return null;
-    }
-
-    @Override
-    public ENT update(ENT entity) {
-        return null;
-    }
-
-    @Override
-    public Iterable<ENT> findAll() {
-        return null;
-    }
 }
