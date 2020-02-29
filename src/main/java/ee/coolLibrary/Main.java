03edbf5f298589e90a708630f219ea2b08e7718c
dmitrii <dmitrii.golikov91@gmail.com>
2020-02-29 10:39:04 +0200

    Repositories structure

--- src/main/java/ee/coolLibrary/Main.java
+++ src/main/java/ee/coolLibrary/Main.java
@@ -1,3 +1,5 @@
+package ee.coolLibrary;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/main/java/ee/coolLibrary/repositories/ReviewRepository.java
@@ -0,0 +1,17 @@
+package ee.coolLibrary.repositories;
+
+import ee.coolLibrary.entities.Review;
+
+public class ReviewRepository implements SimpleRepository <Review, String> {
+    public Review save(Review entity) {
+        return null;
+    }
+
+    public Review findById(String s) {
+        return null;
+    }
+
+    public Iterable<Review> findAll() {
+        return null;
+    }
+}
