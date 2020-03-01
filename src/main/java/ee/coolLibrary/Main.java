0428977552c896cf28fb45baa6ab1835bf049880
dmitrii <dmitrii.golikov91@gmail.com>
2020-03-01 09:55:24 +0200

    add init

--- src/main/java/ee/coolLibrary/Main.java
+++ src/main/java/ee/coolLibrary/Main.java
@@ -1,12 +1,20 @@
 package ee.coolLibrary;
 
+import ee.coolLibrary.entities.Author;
+import ee.coolLibrary.repositories.AuthorRepository;
 import ee.coolLibrary.repositories.DatabaseUtil;
+import ee.coolLibrary.services.AuthorServiceImpl;
+import ee.coolLibrary.services.contracts.AuthorService;
 
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) {
-
+DatabaseUtil.init();
+        AuthorRepository authorRepository = new AuthorRepository();
+        AuthorService authorService = new AuthorServiceImpl(authorRepository);
+       Author saved = authorService.save(new Author("fsdfds","dawdw"));
+        System.out.println(saved);
         DatabaseUtil.shutdown();
     }
 
